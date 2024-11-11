7f555601dee9ba278c2ea068eabbb2613e162c33
Cvely <84121069+c-vely@users.noreply.github.com>
2024-11-11 18:07:16 +0900

    <Feat> UI + 폰트 추가
    - UI + 폰트 추가 완료

---
+++ Source/HttpTest/Public/CJS/CJS_SubObjectActor.h
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "CJS_SubObjectActor.generated.h"
+
+UCLASS()
+class HTTPTEST_API ACJS_SubObjectActor : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ACJS_SubObjectActor();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	UFUNCTION(BlueprintCallable)
+	void InitSubObject(FString objName);
+};
